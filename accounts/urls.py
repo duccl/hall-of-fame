b8beefc393700d4cb3eb1c67a7afb74838689197
duccl <edr.ccol@gmail.com>
2020-07-19 12:30:32 -0300

    LoginView Created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('',SignUpView.as_view(),name='sign-up')
+    path('',SignUpView.as_view(),name='sign-up'),
+    path('login/',CustomLoginView.as_view(),name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from .forms import *
-from django.urls import reverse
+from django.urls import reverse,reverse_lazy
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.views import LoginView,LogoutView
 
 class SignUpView(CreateView):
     form_class = CustomUserCretionForm
@@ -13,3 +14,7 @@ class SignUpView(CreateView):
         login(self.request,user=user)
         if self.request.user.is_authenticated:
             return reverse('halls:dashboard',kwargs={'user_id':user.pk})
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    #success_url = reverse_lazy('halls:home')
