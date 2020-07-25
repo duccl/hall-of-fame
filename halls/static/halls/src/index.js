b40f7fed8f5b31aff90301395453d2865ab83d3b
duccl <edr.ccol@gmail.com>
2020-07-25 11:36:30 -0300

    Populating Search Container

--- halls/static/halls/src/index.js
+++ halls/static/halls/src/index.js
@@ -11,11 +11,24 @@ const callAPI = async (text, callback) => {
 }
 
 const populateSearchContainer =  (data) => {
-    $(id_display_search).text(JSON.stringify(data))
+    console.log(data['items'])
+    data['items'].map(item=>{
+        let video_snippet = item['snippet']
+        let div = document.createElement('div')
+        div.setAttribute('class','card row margin-top-md roboto')
+        div.innerHTML = `${video_snippet['title']} \r\n
+                         ${video_snippet['description']}`
+        div.style.color= 'black'
+        $(id_display_search).append(div)
+    })
 }
 
+let delayTimer;
 $("#id_search_term").keyup(function(){
+    clearTimeout(delayTimer);
     let search_text = $(this).val()
     $(id_display_search).text('Loading...')
-    callAPI(search_text,populateSearchContainer)
+    delayTimer = setTimeout(function(){
+        callAPI(search_text,populateSearchContainer)
+    },1000)
 });
