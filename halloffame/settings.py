aadcd4007a1413c2c6c84b30582d47cc5ab032f0
duccl <edr.ccol@gmail.com>
2020-07-26 22:03:25 -0300

    Heroku Deployment

--- halloffame/settings.py
+++ halloffame/settings.py
@@ -18,7 +18,8 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_KEY')
+# SECRET_KEY = os.getenv('DJANGO_KEY') 
+SECRET_KEY = '%lt-j=i-ix4c-i-&@dn1=f+v59q=h&q1i*&k1db))bn*yu^zh%'
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,4 +127,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
